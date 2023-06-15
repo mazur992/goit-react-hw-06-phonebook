0ae5f6591f3eed3f87f0b5e65c5d057ed727c69f
Mazur Roman <superioritywork@gmail.com>
2023-06-15 21:18:25 +0200

    Update store.js

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -6,14 +6,9 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'localContacts',
   storage,
+  blacklist: ['filter'],
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({ reducer: persistedReducer });
 export const persistor = persistStore(store);
-
-// export default () => {
-//   let store = createStore(persistedReducer);
-//   let persistor = persistStore(store);
-//   return { store, persistor };
-// };
